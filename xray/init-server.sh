4fa9ad7dab9a3d591a00c567290c3baafaf9bee9
hp <aaa.bbb.ccc>
2023-02-18 10:14:43 -0500

    aaa

---
+++ xray/init-server.sh
@@ -0,0 +1,10 @@
+
+apt install lrzsz screen -y
+apt --no-install-recommends -y install wget ca-certificates || (apt update && apt --no-install-recommends -y install wget ca-certificates)
+wget -O Xray-TLS+Web-setup.sh https://github.com/kirin10000/Xray-script/raw/main/Xray-TLS+Web-setup.sh
+#bash Xray-TLS+Web-setup.sh
+
+
+
+
+
